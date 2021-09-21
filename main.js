e0cfea96641a90274ff0113c4a65ce668b5205e2
Dabsia <dabojohnson98@gmail.com>
2021-09-21 11:34:54 +0100

    Made updates to the nav bar

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 var menu = document.getElementById('menu');
 var ul = document.querySelector('ul');
 var logo = document.querySelector('.logo')
-
+var image__container = document.querySelector('.image__container')
 function showNav() {
-    
     ul.classList.toggle('open')
     
     document.body.style.overflow = 'hidden'
