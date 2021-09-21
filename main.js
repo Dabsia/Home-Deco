f679cf3d2a8c5c6d9da789c154d4a883fed7ac1e
Dabsia <dabojohnson98@gmail.com>
2021-09-21 11:40:50 +0100

    Updates on the nav

--- main.js
+++ main.js
@@ -4,7 +4,5 @@ var logo = document.querySelector('.logo')
 var image__container = document.querySelector('.image__container')
 function showNav() {
     ul.classList.toggle('open')
-    
-    document.body.style.overflow = 'hidden'
 }
 menu.addEventListener('click', showNav) 
